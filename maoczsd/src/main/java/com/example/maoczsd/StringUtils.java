dc21069bfff77595143f457613e5c1666daba0a1
Andy <1098055977@qq.com>
2017-06-14 12:19:13 +0800

    ss

---
+++ maoczsd/src/main/java/com/example/maoczsd/StringUtils.java
@@ -0,0 +1,12 @@
+package com.example.maoczsd;
+
+/**
+ * Created by Administrator on 2017/6/14 0014.
+ */
+
+public class StringUtils {
+
+    public static boolean isRight(){
+        return true;
+    }
+}
